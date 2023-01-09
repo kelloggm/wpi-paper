b635e7ea42e572860ad8ea6b4f88b090a36d3cc9
Muyeed Ahmed <akib100095@gmail.com>
2023-01-09 14:02:13 -0500

    Update wpi-template.sh

--- experiments/wpi-template.sh
+++ experiments/wpi-template.sh
@@ -8,7 +8,7 @@
 # Where should the output be placed at the end? This directory is also
 # used to store intermediate WPI results. The directory does not need to
 # exist. If it does exist when this script starts, it will be deleted.
-
+WPITEMPDIR=/tmp
 # Where is WPI's output placed by the Checker Framework? This is some
 # directory ending in build/whole-program-inference. For most projects,
 # this directory is just ./build/whole-program-inference.
