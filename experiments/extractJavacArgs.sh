01828d48e4fb957abdddc06ac74e0c79f3817d64
dan <dd482@njit.edu>
2023-01-08 20:04:19 -0500

    Small script; Modified Compute Annos

---
+++ experiments/extractJavacArgs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+MVN_COMPILE="mvn -X clean compile"
+${MVN_COMPILE} > compile-out.txt
+echo -n '$JAVA_HOME/bin/javac' > JavacRaw.txt
+grep -E '\s-d .*$' compile-out.txt | cut -c 8- >> JavacRaw.txt
+RUN_ANNO_STATS=$(cat JavacRaw.txt)
+eval "${RUN_ANNO_STATS}"
