5056c7e6e1253597b92d013b9e0f8877e59ce654
Daniel Daszkiewicz <dd482@njit.edu>
2023-02-13 10:22:30 -0500

    Added extractJavacArgs.sh

---
+++ experiments/extractJavacArgs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+# This script extracts the javac args of a maven project.
+# It may be useful when annotation statistics swallows expected output 
+# and the project has to be run manually as explained in the notes. 
+# Make sure you have a $JAVA_HOME variable set.
+
+
+MVN_COMPILE="mvn -X clean compile"
+${MVN_COMPILE} > compile-out.txt
+echo -n '$JAVA_HOME/bin/javac' > JavacRaw.txt
+grep -E '\s-d .*$' compile-out.txt | cut -c 8- >> JavacRaw.txt
+RUN_ANNO_STATS=$(cat JavacRaw.txt)
+echo $RUN_ANNO_STATS
