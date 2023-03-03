cf94c5a1f66772a8dc3dd5e22a720f904444cc69
LoiNguyenCS <113363230+LoiNguyenCS@users.noreply.github.com>
2023-03-03 12:19:40 -0500

    remove JAVA_SRC_DIR

--- experiments/format.sh
+++ experiments/format.sh
@@ -3,9 +3,6 @@
 # the root of the wpi output
 WPI_RESULTS_DIR=./wpi-annotations/
 
-# the root of the java source files
-JAVA_SRC_DIR=./src/main/java/
-
 for ajava_file in $(find ${WPI_RESULTS_DIR} -name "*.ajava"); do
     git mv ${ajava_file} ${ajava_file}.java
 done
