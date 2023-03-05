25191d51988097b872710aaa756984ae26f1dc89
Daniel Daszkiewicz <dd482@njit.edu>
2023-03-05 13:33:38 -0500

    skip iterations that do not have .ajava files

--- experiments/inferred-annos-counter/run-iac.sh
+++ experiments/inferred-annos-counter/run-iac.sh
@@ -30,10 +30,11 @@ JAVA_FILES=$(find "${1}" -name "*.java")
 
 for file in ${JAVA_FILES}; do
     echo "${file}:"
-    filenodirname="${file##*/}"
+    filenodirname="${file##*/}" 
     filebasename="${filenodirname%.*}"
     AJAVA_FILES=$(find "${2}" -name "${filebasename}-*.ajava")
     AJAVA_SPACE=$(echo "${AJAVA_FILES}" | tr "\n" " ")
+    if [[ ! ${AJAVA_FILES} == *".ajava"* ]]; then continue; fi
     run_iac "${file}" "${AJAVA_SPACE}"
 done
 
