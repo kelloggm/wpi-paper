488b535718b9303fef8af091794ca2be3e73c913
Daniel Daszkiewicz <dd482@njit.edu>
2023-05-14 22:34:42 -0400

    Base script

--- experiments/auto-wpi-projects.sh
+++ experiments/auto-wpi-projects.sh
@@ -2,41 +2,35 @@
 
 # List of Github repositories to clone
 repositories=(
-    "https://github.com/kelloggm/icalavailable"
-    "https://github.com/dd482IT/require-javadoc-WPI"
-    "https://github.com/dd482IT/randoop-wpi-testing.git"
-    "https://github.com/dd482IT/randoop-wpi.git"
-    "https://github.com/dd482IT/RxNorm-explorer-wpi.git"
+  "https://github.com/kelloggm/icalavailable"
+  "https://github.com/dd482IT/require-javadoc-WPI"
+  "https://github.com/dd482IT/randoop-wpi.git"
+  "https://github.com/dd482IT/table-wrapper-csv-impl-wpi.git"
+  "https://github.com/dd482IT/table-wrapper-csv-impl-wpi.git"
+  "https://github.com/dd482IT/RxNorm-explorer-wpi.git"
+  "https://github.com/dd482IT/randoop-wpi.git"
+  "https://github.com/dd482IT/reflection-util-wpi"
+  "https://github.com/dd482IT/multi-version-control-wpi"
+  "https://github.com/dd482IT/lookup-wpi"
+  "https://github.com/dd482IT/Nameless-Java-API-WPI.git"
 )
 
-# How to handle dmn-check?
-# How to handle randoop?
-
-# Branch to checkout in each repository
 branch="wpi-enabled"
-JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home"
-CHECKERFRAMEWORK="/Users/daniel/Documents/NJIT/WPI-Research/checker-framework/checker-framework-dd482IT"
-#PROJECT_DIR="WPI_EXPERIMENTS" 
-# Make a root directory 
-#mkdir $PROJECT_DIR
-#pushd $PROJECT_DIR
+
+if [ -z $JAVA_HOME ]; then echo "\$JAVA_HOME is empty"; exit; fi; # 
+ # Since all projects support Java 11 (And some require Java 11, set this to JAVA 11)
+
+if [ -z $CHECKERFRAMEWORK ]; then echo "\$JAVA_HOME is empty"; exit; fi; # Only necessary for Gradle Projects, MVN projects will need a local snapshot. 
 
 for repository in "${repositories[@]}"
 do
-    # Extract repository name from URL
     repo_name=$(echo $repository | cut -d'/' -f5 | cut -d'.' -f1)
-
-    # Clone repository
     git clone $repository
     pushd $repo_name
     git checkout $branch
-
-    source wpi.sh 2>&1 logErrors$repo_name.txt # Source will bring in the enviroment variables # How to get the warnings from gradle? 
-    # Run wpi.sh > file 
+    /bin/bash wpi.sh 
     popd
-    exit
 done
-
 
 
 
