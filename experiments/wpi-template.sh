df1c8799d3d2a4eb625bc7f5596d76eb3c407d49
Daniel Daszkiewicz <dd482@njit.edu>
2023-05-18 11:43:38 -0400

    Remove previous starter script; restore wpi-template to original

--- experiments/wpi-template.sh
+++ experiments/wpi-template.sh
@@ -7,16 +7,14 @@
 
 
 # The compile and clean commands for the project's build system.
-BUILD_CMD="mvn compile"
-CLEAN_CMD="mvn clean"
-#${BUILD_CMD} # Compile the program so that WPIOUTDIR is created.
+BUILD_CMD="./gradlew compileJava"
+CLEAN_CMD="./gradlew clean"
+${BUILD_CMD} # Compile the program so that WPIOUTDIR is created.
 
 # Where should the output be placed at the end? This directory is also
 # used to store intermediate WPI results. The directory does not need to
 # exist. If it does exist when this script starts, it will be deleted.
-export WPITEMPDIR="$1"
-set -- $WPITEMPDIR
-
+WPITEMPDIR=tmp
 # Where is WPI's output placed by the Checker Framework? This is some
 # directory ending in build/whole-program-inference. For most projects,
 # this directory is just ./build/whole-program-inference .
@@ -31,9 +29,8 @@ set -- $WPITEMPDIR
 # same build system (e.g., because of a project's settings.gradle file).
 
 # Program needs to compiled before running script so WPI creates this directory.
-WPIOUTDIR="$2" #This currently does not take in the data from the multi-script
-#echo $WPIOUTDIR
-exit
+WPIOUTDIR=~/.gradle/workers/build/whole-program-inference 
+
 # Whether to run in debug mode. In debug mode, output is printed to the terminal
 # at the beginning of each iteration, and the diff between each pair of iterations is
 # saved in a file named iteration$count.diff, starting with iteration1.diff.
@@ -41,6 +38,7 @@ exit
 DEBUG=1
 
 # End of variables. You probably don't need to make changes below this line.
+
 rm -rf ${WPITEMPDIR}
 mkdir -p ${WPITEMPDIR}
 
@@ -53,8 +51,7 @@ while : ; do
     ${CLEAN_CMD}
     # This mkdir is needed when the project has subprojects.
     mkdir -p "${WPITEMPDIR}"
-    echo ${WPITEMPDIR}
-    echo ${WPIOUTDIR}
+    mkdir -p "${WPIOUTDIR}"
     DIFF_RESULT=$(diff -r ${WPITEMPDIR} ${WPIOUTDIR})
     if [[ ${DEBUG} == 1 ]]; then
 	echo "putting the diff for iteration $count into $(realpath iteration$count.diff)"
