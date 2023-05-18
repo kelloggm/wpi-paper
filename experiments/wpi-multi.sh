917062def3a2d87e1cd857a9cf8783452efd6ff1
Daniel Daszkiewicz <dd482@njit.edu>
2023-05-18 11:39:35 -0400

    Finalized code

--- experiments/wpi-multi.sh
+++ experiments/wpi-multi.sh
@@ -1,56 +1,38 @@
 #!/bin/bash
 
-# Have list of the project sub names (ie, Project1, Project2, Project3)
+# This script is designed to run WPI on projects that have multiple sub projects. These subprojects usally are sub directories within the project space.
 
-# Check if project name exists
-if [ -z "$1" ] 
-then 
-    echo "Usage: <Projectname> <ProjectsFile>" 
-    exit
-else 
-    PROJECT_NAME="$1"
-fi
+# This array requires human input.
+SUBPROJECTS=(
+    "core"                                                    
+    "validators"                                              
+    "dmn-check-server"                                                   
+    "plugin-base"                                              
+    "maven-plugin"
+    "cli"
+)
 
-if [ -z "$2" ] 
-then 
-    echo "Usage: <Projectname> <ProjectsFile>" 
-    exit
-fi
+# Full path to the souece project or repo on the system.
+PROJECT_SPACE="/Users/daniel/Documents/NJIT/WPI-Research/experiments-live/tmp-storage/dmn-check-wpi" 
 
-TOP_LEVEL=~/Documents/WPITEMP-${PROJECT_NAME} #This directory should be chosen by the user?
+# The top level directory is the root directory of the project as a whole. A directory should be supplied by the user but tmp should do fine for now.
+TOP_LEVEL=~/Documents/WPITEMP-${PROJECT_NAME} 
 mkdir -p ${TOP_LEVEL}
 
 # Check if all sub-project directories exist and contain java files 
-while read subProject; do
-    if [ -d ./$subProject ]
+for subProject in read "${SUBPROJECTS[@]}"
+do
+    if [ -d $PROJECT_SPACE/$subProject ]
     then
         echo "$subProject Found"
         mkdir -p ${TOP_LEVEL}/${subProject}
+        WPITEMPDIR=${TOP_LEVEL}/${subProject}
+        echo "Running WPI ON $subProject"
+        echo $WPIOUTDIR
+        ./wpi-multi.sh $WPITEMPDIR $WPIOUTDIR
+        wait
     else 
-        echp "$subProject Missing"
+        echo "$subProject Missing"
         exit
     fi
-done <$2
-
-while read subProject; do
-    WPIOUTDIR="./${subProject}/build/" #This should not assume that the files will be found in build but it is most likely they will
-    #For some reason ".${subproject}/build/ doesn't work when passing to wpi script"
-    WPITEMPDIR=${TOP_LEVEL}/${subProject}
-    echo "Running WPI ON $subProject"
-    echo $WPIOUTDIR
-    ./wpi-multi.sh $WPITEMPDIR $WPIOUTDIR
-    wait
-done <$2
-
-
-
-
-#WPIOUTDIRECTORY 
-#WPITEMPDIRECTORY 
-
-# This script will iterate over each project name (ie, Project1, Project2, Project3)
-# It will create a root WPITEMPDIR (ie, /tmp/Project/)
-# It will pass the root dir + the specific project dir to WPI and to pom.xml as a variable 
-
-# wpi.sh (#WPITEMPDIR = root + sub dir) (WPIOUTDIRECTORY = ./subdir + /build/
-# This will iterate until the wpi script returns a status code of 0 which will then continue on to the next project
+done
