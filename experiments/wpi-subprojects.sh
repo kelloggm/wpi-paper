8336ac146b1c4c75d1f5c2a83171bad87994e669
Daniel Daszkiewicz <dd482@njit.edu>
2023-05-19 11:55:58 -0400

    Added requested changes

--- experiments/wpi-subprojects.sh
+++ experiments/wpi-subprojects.sh
@@ -3,36 +3,55 @@
 # This script runs wpi.sh on projects that contain multiple  
 # sub projects under their respective directories within your project.
 
-# The changes you will have to make include:
+# You must make the following changes:
 # 1. Add the name of each subproject directory to the `SUBPROJECTS` array. 
 # 2. Change WPITEMPDIR to "$1" and WPIOUTDIR to "$2" in your copy 
-# of the wpi-template.sh script.
+#    of the wpi-template.sh script.
 # 3. Set -Ajava=${env.WPITEMPDIR} in the pom.xml.
 # 4. Run the script using the `source` command.
+
+# Other changes (to this script, the wpi.sh script, the project's 
+# build system, or any combination of the former) may also 
+# be required, to account for project-specific complexity.
+
+
+# This variable is where the WPI Project root directory will be made and will store the entire project output (PROJECT_TEMP_ROOT).
+TOP_LEVEL=/tmp
+
+
+# Full path to the source project/repo on the system.
+PROJECT_SPACE=$(realpath)
+PROJECT_NAME=$(basename "${PROJECT_SPACE}")
+PROJECT_TEMP_ROOT="${TOP_LEVEL}"/"${PROJECT_NAME}"
+
+mkdir "${PROJECT_TEMP_ROOT}"
 
 SUBPROJECTS=(
 # Add the subdirectory names here.
 )
 
-# Full path to the source project/repo on the system.
-PROJECT_SPACE=$(realpath)
-PROJECT_NAME=$(basename "${PROJECT_SPACE}")
-# The root directory where all the subprojects WPITEMP directories will be stored, change tmp/ as needed.
-TOP_LEVEL=/tmp/"${PROJECT_NAME}"
-mkdir "${TOP_LEVEL}"
-
+NO_MISSING_PROJECTS=true
 for subProject in "${SUBPROJECTS[@]}"
 do
     if [ -d "${PROJECT_SPACE}"/"${subProject}" ]
     then
-        echo "$subProject Found"
-        mkdir -p "${TOP_LEVEL}"/"${subProject}"
-        WPITEMPDIR="${TOP_LEVEL}"/"${subProject}"
-        WPIOUTDIR="$PROJECT_SPACE/$subProject/build/"
-        echo "Running WPI ON $subProject"
-        # Change the name of your wpi.sh script if needed.
-        ./wpi.sh "${WPITEMPDIR}" "${$WPIOUTDIR}"   
+        echo "$subProject found"
     else 
-        echo "$subProject Missing"
-    fi
+        echo "$subProject not found"
+        NO_MISSING_PROJECTS=false
+    fi 
+
+    if [ "${NO_MISSING_PROJECTS}" = false ] ; then 
+        echo "Subprojects not found, check your array list.";
+        return
+    fi 
+done 
+
+for subProject in "${SUBPROJECTS[@]}"
+do
+    mkdir -p "${PROJECT_TEMP_ROOT}"/"${subProject}"
+    WPITEMPDIR="${PROJECT_TEMP_ROOT}"/"${subProject}"
+    WPIOUTDIR="$PROJECT_SPACE/$subProject/build/"
+    echo "Running WPI ON $subProject"
+    ./wpi.sh $WPITEMPDIR $WPIOUTDIR
 done
