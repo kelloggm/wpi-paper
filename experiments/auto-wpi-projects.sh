c6551e5d7324b7ac9c0ad11c85acb6f40025395a
Daniel Daszkiewicz <dd482@njit.edu>
2023-05-31 05:52:46 -0400

    Ran shellcheck; ignored pushd warning

--- experiments/auto-wpi-projects.sh
+++ experiments/auto-wpi-projects.sh
@@ -9,7 +9,7 @@ repositories=(
   "https://github.com/dd482IT/dmn-check-wpi.git"
   "https://github.com/kelloggm/icalavailable" 
   "https://github.com/dd482IT/require-javadoc-WPI"
-  "https://github.com/dd482IT/randoop-wpi.git"
+  #"https://github.com/dd482IT/randoop-wpi.git"
   "https://github.com/dd482IT/table-wrapper-csv-impl-wpi.git"
   "https://github.com/dd482IT/table-wrapper-api-WPI.git"
   "https://github.com/dd482IT/RxNorm-explorer-wpi.git"
@@ -23,22 +23,21 @@ PROJECT_DIR="projects"
 PROJECT_PATH=$(realpath $PROJECT_DIR)
 # Root directory for all projects.
 if [ ! -d $PROJECT_DIR ]; then mkdir $PROJECT_DIR; fi 
-pushd $PROJECT_DIR 
+pushd "$PROJECT_DIR"
 
 # Java home should be set to the path for JAVA 11 as some projects only run under Java 11.
 if [ -z "$JAVA_HOME" ]; then echo "JAVA_HOME is empty"; exit; fi; 
 # This path will only be used by gradle projects running them with the -PcfLocal argument.
-if [ -z "$CHECKERFRAMEWORK" ]; then echo "An error has occured with the checkerframework path"; exit; fi; # Only necessary for Gradle Projects, MVN projects will need a local snapshot. 
-
+if [ -z "$CHECKERFRAMEWORK" ]; then echo "An error has occured with the checkerframework path"; exit; fi; 
 for repository in "${repositories[@]}"
 do
   # Default
   BRANCH="wpi-enabled"
   WPI_SCRIPT="wpi.sh"
-  repo_name=$(echo $repository | cut -d'/' -f5 | cut -d'.' -f1)
+  repo_name=$(echo "$repository" | cut -d'/' -f5 | cut -d'.' -f1)
   # Chane script name for dmn-check
-  git clone $repository
-  pushd $repo_name
+  git clone "$repository"
+  pushd "$repo_name"
   # Traverse to subproject [Special Case Project]
   if [ "$repo_name" = "cache2k-wpi" ]; then pushd "cache2k-api"; fi
   git checkout $BRANCH
@@ -54,5 +53,5 @@ do
   ./$WPI_SCRIPT 
   wait
   # Return back to home project directory.
-  pushd $PROJECT_PATH
+  pushd "$PROJECT_PATH"
 done
